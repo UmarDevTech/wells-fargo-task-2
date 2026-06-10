b2d544357c64748750686147bd5986e4ceca9bd9
UmarDevTech <shakeelumar2005@gmail.com>
2026-06-10 12:17:57 +0100

    Update Portfolio.java

--- src/main/java/entities/Portfolio.java
+++ src/main/java/entities/Portfolio.java
@@ -1,1 +1,21 @@
+package entities;
 
+import javax.persistence.*;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    @OneToOne
+    private Client client;
+
+    public Portfolio() {}
+
+    public Portfolio(Long portfolioId, Client client) {
+        this.portfolioId = portfolioId;
+        this.client = client;
+    }
+}
