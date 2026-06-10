8983ae42434b54ce581072d986243e380f4481a5
UmarDevTech <shakeelumar2005@gmail.com>
2026-06-10 12:16:40 +0100

    Update Advisor.java

--- src/main/java/entities/Advisor.java
+++ src/main/java/entities/Advisor.java
@@ -1,1 +1,30 @@
+package entities;
 
+import javax.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    @ManyToOne
+    private Advisor advisor;
+
+    public Client() {}
+
+    public Client(Long clientId, String firstName, String lastName, String email, String phone, Advisor advisor) {
+        this.clientId = clientId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.advisor = advisor;
+    }
+}
