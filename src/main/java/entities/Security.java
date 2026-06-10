2b677ad3fe1814097037b0fcfeefa75954b8a5b9
UmarDevTech <shakeelumar2005@gmail.com>
2026-06-10 12:18:22 +0100

    Update Security.java

--- src/main/java/entities/Security.java
+++ src/main/java/entities/Security.java
@@ -1,1 +1,35 @@
+package entities;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long securityId;
+
+    private String name;
+    private String category;
+    private LocalDate purchaseDate;
+    private double purchasePrice;
+    private int quantity;
+
+    @ManyToOne
+    private Portfolio portfolio;
+
+    public Security() {}
+
+    public Security(Long securityId, String name, String category,
+                    LocalDate purchaseDate, double purchasePrice,
+                    int quantity, Portfolio portfolio) {
+        this.securityId = securityId;
+        this.name = name;
+        this.category = category;
+        this.purchaseDate = purchaseDate;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+        this.portfolio = portfolio;
+    }
+}
